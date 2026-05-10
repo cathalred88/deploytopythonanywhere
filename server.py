bd7babb4d980e1898003b40039a243ec83a984a7
Cathal <cathalred88@hotmail.com>
2026-05-10 22:50:59 +0100

    update server message for test

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return 'test to chekck if the server is running new code'
 
 if __name__ == '__main__':
     app.run(debug=True)
